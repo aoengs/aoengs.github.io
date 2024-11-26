735fdbb677a079f7d531b23b1b61fbe8b0c02fb9
aoengs <haofengshang@gmail.com>
2024-11-26 09:50:10 +0800

    优化

--- src/data/apps.ts
+++ src/data/apps.ts
@@ -1,91 +1,90 @@
 export const categories = [
-    { id: 'all', name: '全部应用', icon: '🤓' },
-    { id: 'AI', name: 'AI 工具', icon: '🤖' },
-    { id: 'Web', name: ' 前端开发', icon: '🎨' },
-    { id: 'design', name: '设计工具', icon: '✏️' },
-    { id: 'image', name: '图像处理', icon: '🖼️' },
-    { id: 'video', name: '视频制作', icon: '🎥' },
-    { id: 'Github', name: 'Github', icon: '✨' } ,
-   
+  { id: "all", name: "全部应用", icon: "🤓" },
+  { id: "AI", name: "AI 工具", icon: "🤖" },
+  { id: "Web", name: " 前端开发", icon: "🎨" },
+
+  { id: "image", name: "图像处理", icon: "🖼️" },
+
+  { id: "Github", name: "Github", icon: "✨" },
 ];
-// { id: 'blog', name: ' 我的博客', icon: '✨' }
+// { id: 'blog', name: ' 我的博客', icon: '✨' }  { id: 'design', name: '设计工具', icon: '✏️' },    { id: 'video', name: '视频制作', icon: '🎥' },
 export const apps = [
-    {
-        name: "千古前端图文教程",
-        icon: "🎨",
-        url: "https://web.qianguyihao.com/",
-        category: "Web",
-        desc: "从零开始学前端"
-    },
-    {
-        name: "开发者项目列表",
-        icon: "🎯",
-        url: "https://github.com/1c7/chinese-independent-developer",
-        category: "Github",
-        desc: "聚合所有中国独立开发者的项目"
-    },
-    {
-        name: "Unsplash",
-        icon: "🎯",
-        url: "https://unsplash.com",
-        category: "image",
-        desc: "高清图片素材库"
-    },
-    {
-        name: "ollama",
-        icon: "🎯",
-        url: "https://github.com/ollama/ollama",
-        category: "Github",
-        desc: "开源AI模型库"
-    },
-    {
-        name: "Qwerty-Learner",
-        icon: "🎯",
-        url: "https://github.com/RealKai42/qwerty-learner",
-        category: "Github",
-        desc: "单词打字练习"
-    },
-    {
-        name: "Moki",
-        icon: "🎯",
-        url: "https://www.moki.cn/",
-        category: "AI",
-        desc: "用 AI 做短视频"
-    },
-    {
-        name: "Kimi",
-        icon: "🎯",
-        url: "https://kimi.moonshot.cn/",
-        category: "AI",
-        desc: "会推理解析，能深度思考的 AI 助手"
-    },
-    ,
-    {
-        name: "豆包",
-        icon: "🎯",
-        url: "https://www.doubao.com/chat/",
-        category: "AI",
-        desc: "字节跳动旗下 AI 助手"
-    },
-    {
-        name: "undraw",
-        icon: "🎯",
-        url: "https://undraw.co/illustrations",
-        category: "image",
-        desc: "免费 SVG 插画素材"
-    },
-    {
-        name: "通义千问",
-        icon: "🎯",
-        url: "https://tongyi.aliyun.com/qianwen/",
-        category: "AI",
-        desc: "阿里云出品，支持 AI 编码"
-    },
-    {
-        name: "文心一言",
-        icon: "🎯",
-        url: "https://yiyan.baidu.com",
-        category: "AI",
-        desc: "百度出品，AI 聊天机器人"
-    }
-]; 
+  {
+    name: "千古前端图文教程",
+    icon: "🎨",
+    url: "https://web.qianguyihao.com/",
+    category: "Web",
+    desc: "从零开始学前端",
+  },
+  {
+    name: "开发者项目列表",
+    icon: "🎯",
+    url: "https://github.com/1c7/chinese-independent-developer",
+    category: "Github",
+    desc: "聚合所有中国独立开发者的项目",
+  },
+  {
+    name: "Unsplash",
+    icon: "🎯",
+    url: "https://unsplash.com",
+    category: "image",
+    desc: "高清图片素材库",
+  },
+  {
+    name: "ollama",
+    icon: "🎯",
+    url: "https://github.com/ollama/ollama",
+    category: "Github",
+    desc: "开源AI模型库",
+  },
+  {
+    name: "Qwerty-Learner",
+    icon: "🎯",
+    url: "https://github.com/RealKai42/qwerty-learner",
+    category: "Github",
+    desc: "单词打字练习",
+  },
+  {
+    name: "Moki",
+    icon: "🎯",
+    url: "https://www.moki.cn/",
+    category: "AI",
+    desc: "用 AI 做短视频",
+  },
+  {
+    name: "Kimi",
+    icon: "🎯",
+    url: "https://kimi.moonshot.cn/",
+    category: "AI",
+    desc: "会推理解析，能深度思考的 AI 助手",
+  },
+  ,
+  {
+    name: "豆包",
+    icon: "🎯",
+    url: "https://www.doubao.com/chat/",
+    category: "AI",
+    desc: "字节跳动旗下 AI 助手",
+  },
+  {
+    name: "undraw",
+    icon: "🎯",
+    url: "https://undraw.co/illustrations",
+    category: "image",
+    desc: "免费 SVG 插画素材",
+  },
+  {
+    name: "通义千问",
+    icon: "🎯",
+    url: "https://tongyi.aliyun.com/qianwen/",
+    category: "AI",
+    desc: "阿里云出品，支持 AI 编码",
+  },
+  {
+    name: "文心一言",
+    icon: "🎯",
+    url: "https://yiyan.baidu.com",
+    category: "AI",
+    desc: "百度出品，AI 聊天机器人",
+  },
+];
